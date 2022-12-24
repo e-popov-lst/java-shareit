89c3d64f8e95a0480af2c92df460724812dcb16e
Eugene Popov <eugene.popov@list.ru>
2022-12-24 03:09:11 +0300

    fix: спринт 13 - ShareIt. Часть 1 - unit-test

--- src/test/java/ru/practicum/shareit/item/ItemServiceTest.java
+++ src/test/java/ru/practicum/shareit/item/ItemServiceTest.java
@@ -19,36 +19,36 @@ public class ItemServiceTest {
     @Test
     @Order(1)
     void findAll() {
-        itemService.getUserStorage().create(new User(101L, "user1", "user1@test.com"));
-        itemService.create(101L, new Item(1L, "item1", "item1 desc", true, null, null));
-        itemService.create(101L, new Item(2L, "item2", "item2 desc", true, null, null));
+        itemService.getUserStorage().create(new User(101L, "user1", "user1@mytest.com"));
+        itemService.create(101L, new Item(1001L, "item1", "item1 desc", true, null, null));
+        itemService.create(101L, new Item(1002L, "item2", "item2 desc", true, null, null));
 
         assertTrue(itemService.findAll(101L).size() >= 2);
     }
 
     @Test
     void create() {
-        itemService.getUserStorage().create(new User(102L, "user2", "user2@test.com"));
-        ItemDto item3 = itemService.create(102L, new Item(3L, "item3", "item3 desc", true, null, null));
+        itemService.getUserStorage().create(new User(102L, "user2", "user2@mytest.com"));
+        ItemDto item3 = itemService.create(102L, new Item(1003L, "item3", "item3 desc", true, null, null));
 
         assertEquals("item3", item3.getName());
     }
 
     @Test
     void update() {
-        User user4 = itemService.getUserStorage().create(new User(104L, "user4", "user4@test.com"));
-        ItemDto item4 = itemService.create(104L, new Item(4L, "item4", "item4 desc", true, null, null));
-        item4 = itemService.update(new Item(4L, "item42", "item4 desc", true, user4, null));
+        User user4 = itemService.getUserStorage().create(new User(104L, "user4", "user4@mytest.com"));
+        ItemDto item4 = itemService.create(104L, new Item(1004L, "item4", "item4 desc", true, null, null));
+        item4 = itemService.update(new Item(1004L, "item42", "item4 desc", true, user4, null));
 
         assertEquals("item42", item4.getName());
     }
 
     @Test
     void updatePart() {
-        User user5 = itemService.getUserStorage().create(new User(105L, "user5", "user5@test.com"));
-        ItemDto item5 = itemService.create(105L, new Item(5L, "item5", "item5 desc", true, null, null));
+        User user5 = itemService.getUserStorage().create(new User(105L, "user5", "user5@mytest.com"));
+        ItemDto item5 = itemService.create(105L, new Item(1005L, "item5", "item5 desc", true, null, null));
 
-        item5 = itemService.updatePart(105L, 5L, new Item(null, "item52", null, true, user5, null));
+        item5 = itemService.updatePart(105L, 1005L, new Item(null, "item52", null, true, user5, null));
 
         assertEquals("item52", item5.getName());
         assertEquals("item5 desc", item5.getDescription());
--- src/test/java/ru/practicum/shareit/user/UserServiceTest.java
+++ src/test/java/ru/practicum/shareit/user/UserServiceTest.java
@@ -18,46 +18,46 @@ public class UserServiceTest {
     @Test
     @Order(1)
     void findAll() {
-        userService.create(new User(1L, "user1", "user1@test.com"));
-        userService.create(new User(2L, "user2", "user2@test.com"));
+        userService.create(new User(1001L, "user1", "user1@test.com"));
+        userService.create(new User(1002L, "user2", "user2@test.com"));
 
         assertTrue(userService.findAll().size() >= 2);
     }
 
     @Test
     void create() {
-        UserDto user3 = userService.create(new User(3L, "user3", "user3@test.com"));
+        UserDto user3 = userService.create(new User(1003L, "user3", "user3@test.com"));
 
         assertEquals("user3", user3.getName());
     }
 
     @Test
     void update() {
-        UserDto user4 = userService.create(new User(4L, "user4", "user4@test.com"));
-        user4 = userService.update(new User(4L, "user4", "user42@test.com"));
+        UserDto user4 = userService.create(new User(1004L, "user4", "user4@test.com"));
+        user4 = userService.update(new User(1004L, "user4", "user42@test.com"));
 
         assertEquals("user42@test.com", user4.getEmail());
     }
 
     @Test
     void updatePart() {
-        UserDto user5 = userService.create(new User(5L, "user5", "user5@test.com"));
-        user5 = userService.updatePart(5L, new User(null, "user52", null));
+        UserDto user5 = userService.create(new User(1005L, "user5", "user5@test.com"));
+        user5 = userService.updatePart(1005L, new User(null, "user52", null));
 
         assertEquals("user52", user5.getName());
     }
 
     @Test
     void delete() {
-        UserDto user6 = userService.create(new User(6L, "user6", "user6@test.com"));
-        UserDto user7 = userService.create(new User(7L, "user7", "user7@test.com"));
-        assertEquals(7L, user7.getId());
+        UserDto user6 = userService.create(new User(1006L, "user6", "user6@test.com"));
+        UserDto user7 = userService.create(new User(1007L, "user7", "user7@test.com"));
+        assertEquals(1007L, user7.getId());
 
-        userService.delete(7);
+        userService.delete(1007);
 
         user7 = userService.findUserById(7);
 
         assertTrue(user7 == null);
-        assertEquals(6L, user6.getId());
+        assertEquals(1006L, user6.getId());
     }
 }
